eb563e3f4a4a30ba5bbb05c814c69613c3ebf70e
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2024-04-27 20:20:15 +0200

    Starting new project! Working on design!

--- src/main/java/org/online/reminder/plannerapp/config/SecurityConfig.java
+++ src/main/java/org/online/reminder/plannerapp/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +21,10 @@ public class SecurityConfig {
                         .requestMatchers("/", "/error").permitAll()
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
